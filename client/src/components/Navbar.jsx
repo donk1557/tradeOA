c0e9b74eb7bca5322790cddf2e42198856a0b643
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-27 12:49:45 -0500

    searchicon

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Search} from '@material-ui/icons';
+
+const Container = styled.div`
+    height: 60px;
+`
+
+const Wrapper =styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+    flex: 1;
+`
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+    border: 1 solid lightgrey;
+`
+
+const Center = styled.div`
+    flex: 1;
+`
+
+const Right = styled.div`
+    flex:1;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+              <Language>EN</Language>
+              <SearchContainer>
+                input 
+                <Search/>
+              </SearchContainer>
+            </Left>
+            <Center>center</Center>
+            <Right>right</Right>
+
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
