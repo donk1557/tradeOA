d761b571dd4eb84327b49c4f04f4ceac2a2163c6
Sun <solskinartwork@gmail.com>
2021-11-30 16:44:32 -0500

    working on authentication

--- paymentApi/index.js
+++ paymentApi/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -11,7 +12,8 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err);
 });
 app.use(express.json());
-app.use ("/api/users", userRoute);
+app.use ("/api/users", authRoute);
+app.use ("/api/auth", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("backend working");
--- paymentApi/routes/auth.js
+++ paymentApi/routes/auth.js
@@ -1,20 +1,42 @@
 const router = require("express").Router();
 const User = ("../models/User")
+const CryptoJS = require("crypto-js")
+
+
 //register
 router.post("/register", async (req,res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_W).toString(),
+        ​
+       
         
     });
     try{
     const savedUser = await newUser.save();
-    res.status(200)
+    res.status(201).json(savedUser);
     
 } catch(err){
     res.status(500).json(err);
 }
+});
+
+//log
+
+router.post("/login", async (req, res)=>{
+    try{
+        const user = await User.findOne({username:req.body.username});
+        !user && res.status(401).json("wrong creditials");
+        const hashpass = CryptoJS.AES.decrypt(user.password, process.env.PASS_W);
+        const password1 = hashpass.toString(CryptoJS.enc.Utf8);
+
+        password1 !== req.body.password && res.status(401).json("wrong creditials");
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    } catch(err){
+        res.status(500).json(err);
+    }
 });
 
 
