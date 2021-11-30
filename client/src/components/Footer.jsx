bca4a5a8325fbd44266aa35ad5674b894bcfb932
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-30 14:35:17 -0500

    newsletter

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const Footer = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Footer
