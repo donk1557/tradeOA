9c5cdc5a1efc0f596742a1867ded65ec30c308e3
Sun <solskinartwork@gmail.com>
2021-11-30 19:21:07 -0500

    backend products

--- paymentApi/routes/user.js
+++ paymentApi/routes/user.js
@@ -26,19 +26,57 @@ router.delete("/:id", verifyTokenandAuth, async (req,res)=>{
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
 
 //get
-router.get("/:id", verifyTokenandAdmin, async (req,res)=>{
+router.get("/find:id", verifyTokenandAdmin, async (req,res)=>{
     try{
-      const user =  await User.findById(req.params.id)
+      const user = await User.findById(req.params.id)
       const {password, ...others} = user._doc;
 
-      res.status(200).json({...others});
+      res.status(200).json(others);
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
+
+//get all
+router.get("/", verifyTokenandAdmin, async (req,res)=>{
+    const query = req.query.new;
+    try{
+      const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find();
+    res.status(200).json(users);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//get stats
+
+router.get("/stats", verifyTokenandAdmin, async (req,res)=>{
+    const date = new Date();
+    const lastY = new Date(date.setFullYear(date.getFullYear()-1));
+    try{
+        const data = await User.aggregate([
+            {$match: {createdAt:{$gte:last}}},
+            {
+                $project:{
+                    month:{$month: "$createdAt"},
+                },
+            },
+            {
+                $group : {
+                    _id: "$month",
+                    total: {$sum:1},
+                },
+            },
+        ]);
+
+        res.status(200).json(data)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
 
